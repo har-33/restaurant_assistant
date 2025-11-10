9df030d0af43bd067495d077a60d3f130603e692
Harshad R <harshadr871@gmail.com>
2025-11-10 10:49:18 +0530

    changing in mic,speak,listen

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,22 @@
-import pyttsx3
-import speech_recognition as sr
+#import pyttsx3
+#import speech_recognition as sr
 
-def speak(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 170)
-    engine.say(text)
-    engine.runAndWait()
+#◘def speak(text):
+#    engine = pyttsx3.init()
+#    engine.setProperty('rate', 170)
+#    engine.say(text)
+#    engine.runAndWait()
 
-def listen():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("🎤 Listening...")
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
-    try:
-        return r.recognize_google(audio)
-    except:
-        return ""
+#def listen():
+#   r = sr.Recognizer()
+#   with sr.Microphone() as source:
+#       print("🎤 Listening...")
+#       r.adjust_for_ambient_noise(source)
+#       audio = r.listen(source)
+#   try:
+#       return r.recognize_google(audio)
+#   except:
+#       return ""
 
 def chatbot_reply(text):
     text = text.lower()
